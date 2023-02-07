d69a81d15dc9ce791f8cd6d3b32fd5e0a0ca3e05
HSManoj <manojmanu7474@gmail.com>
2023-02-07 17:17:47 +0530

    js functionalities complete

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("testing")
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream(){
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+          videoElement.play();
+        };
+        
+    } catch (error) {
+        console.log("something is wrong here",error);
+    }
+
+    button.addEventListener('click', async () =>{
+        // disable button
+        button.disabled = true;
+        // start picture in picture
+        await videoElement.requestPictureInPicture();
+        // reset buttn
+        button.disabled = false;
+
+
+    })
+
+}
+
+selectMediaStream();
